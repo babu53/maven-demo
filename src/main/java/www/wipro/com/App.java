ad5ef32ef7ca47b9040ab538f6382239181bf3a1
babu53 <kishore.t533@gmail.com>
2020-11-18 19:51:25 +0530

    Update App.java

--- src/main/java/www/wipro/com/App.java
+++ src/main/java/www/wipro/com/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! aasdfasfdqwefsdfasdfasdfasddfddafgasdvgadg" );
+        System.out.println( "Hello World! aasdfasfdqwefsdfasdfasdfasddfddafgasdvgadg" )
     }
 }
